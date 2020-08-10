ccdd95f81434b98ad50310a7617f70c16b2a4f80
Alex Krivolutsky <irramail@gmail.com>
2020-08-11 01:50:43 +0700

    load from storage and check all needed checkboxes

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -2,8 +2,10 @@ package tutorial.webapp
 
 import org.scalajs.dom
 import org.scalajs.dom.document
+import org.scalajs.dom.ext._
 import org.scalajs.dom.html
 import scala.scalajs.js.annotation.JSExportTopLevel
+
 
 object TutorialApp {
   def appendPar(targetNode: dom.Node, text: String): Unit = {
@@ -19,14 +21,18 @@ def addClickedMessage(): Unit = {
 
 @JSExportTopLevel("set")
 def set(): Unit = {
-  println(document.getElementById("alt"))
+  var ares = ""
+  document.querySelectorAll("input[type='checkbox']:checked").map(x => ares += x.asInstanceOf[html.Input].value + ";")
+
+  ares = ares.stripSuffix(";").trim
+
   val settings = document.getElementById("url").asInstanceOf[html.Input].value + '|' +
   document.getElementById("fname").asInstanceOf[html.Input].value + '|' +
     document.getElementById("description").asInstanceOf[html.Input].value + '|' +
     document.getElementById("alt").asInstanceOf[html.Input].value + '|' +
     document.getElementById("metaName").asInstanceOf[html.Input].value + '|' +
     document.getElementById("metaDescription").asInstanceOf[html.Input].value + '|' +
-    document.getElementById("alt").asInstanceOf[html.Input].value;
+    ares;
   dom.window.localStorage.setItem(
       "settings", settings
     )
@@ -37,8 +43,7 @@ def main(args: Array[String]): Unit = {
     setupUI()
 
     val u="_"
-
-    val ares = "1:1_320x320,640x640,1280x1280,1920x1920;4:3_320x240,640x480,1280x960,1920x1440;16:9_320x180,640x360,854x480,1280x720,1920x1080";
+    val  ares = "1:1_320x320,640x640,1280x1280,1920x1920;4:3_320x240,640x480,1280x960,1920x1440;16:9_320x180,640x360,854x480,1280x720,1920x1080"
     ares.split(";").foreach( ar => {
       val item_ar = ar.split('_')
       val aa = item_ar(0)
@@ -46,6 +51,15 @@ def main(args: Array[String]): Unit = {
         appendCheckBox(s"$aa$u$res")
       })
     appendBr()
+    })
+
+    var checked = dom.window.localStorage.getItem("settings").split('|')(6)
+    checked.split(";").foreach( ar => {
+      val item_ar = ar.split('_')
+      val aa = item_ar(0)
+      item_ar(1).split(',').foreach( res => {
+        document.getElementById(s"$aa$u$res").setAttribute("checked", "")
+      })
     })
 
     appendInput("url", "50", "https://")
@@ -65,7 +79,7 @@ def main(args: Array[String]): Unit = {
     appendTextarea()
     appendBr()
     appendDiv()
-      println(document.getElementById("alt").asInstanceOf[html.Input].value)
+      //println(document.getElementById("alt").asInstanceOf[html.Input].value)
   })
 }
 
@@ -108,7 +122,7 @@ def appendCheckBox(id: String): Unit = {
   checkBox.setAttribute("id", id)
   checkBox.setAttribute("name", "resolution_"+id)
   checkBox.setAttribute("value", id)
-  checkBox.setAttribute("checked", "")
+  //checkBox.setAttribute("checked", "")
   checkBox.setAttribute("class", "res")
   checkBox.setAttribute("style", "transform: scale(1.4); padding-top:10px;")
 
